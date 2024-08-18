e4e565745f2d877d9b97b6cd1c1909d6bb176835
fsantsil <felipetssilva@hotmail.com>
2024-08-18 21:59:23 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "rg_name" {
+    type = "string"
+    default = "My RG"
+  
+}
+
+variable "location" {
+    type = "string"
+    default = "us east"
+  
+}
+variable "storageAccount" {
+    type = "string"
+
+    default = "sa name"
+  
+}
+variable "environment" {
+    type = "string"
+
+    default = "my env"
+  
+}
